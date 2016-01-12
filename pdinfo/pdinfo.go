391cba77655aff73ea5b0eb20500bdbcaf9b9050
Raffaele Sena <raff367@gmail.com>
2016-01-12 13:07:16 -0800

    A simple program to print the structure of a PDF file

---
+++ pdinfo/pdinfo.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+        "os"
+
+	"github.com/raff/pdfreader/pdfread"
+)
+
+// Example program for pdfread.go
+
+// The program takes a PDF file as argument and recursively dump the content.
+
+func printobj(pd *pdfread.PdfReaderT, o []byte, indent, prefix string) {
+    l := len(o)
+
+    if l > 2 {
+        if o[l-2] == ' ' && o[l-1] == 'R' { // reference
+            o = pd.Obj(o)
+        }
+    }
+
+    if l == 0 {
+        fmt.Printf("%s%s %s\n", indent, prefix, "<<empty>>")
+        return
+    }
+
+    /*
+    if l < 2 {
+        fmt.Printf("%s%s %s\n", indent, prefix, "<<invalid>>")
+        return
+    }
+    */
+
+    switch o[0] {
+    case '[': // array
+        a := pdfread.Array(o)
+
+        fmt.Printf("%s%s %s\n", indent, prefix, "[")
+        indent += "  "
+
+        for i, v := range a {
+            printobj(pd, v, indent, fmt.Sprintf("%2d:", i))
+        }
+
+        indent = indent[2:]
+        fmt.Printf("%s]\n", indent)
+
+    case '<': // dictionary
+        d := pdfread.Dictionary(o)
+        
+        fmt.Printf("%s%s %s\n", indent, prefix, "{")
+        indent += "  "
+
+        for k, v := range d {
+            if k == "/Parent" { // backreference - don't follow
+                fmt.Printf("%s%s <<%s>>\n", indent, k, string(v))
+            } else {
+                printobj(pd, v, indent, k)
+            }
+        }
+
+        indent = indent[2:]
+        fmt.Printf("%s}\n", indent)
+
+    default:
+        fmt.Printf("%s%s %s\n", indent, prefix, string(o))
+    }
+}
+
+func main() {
+    for _, f := range os.Args[1:] {
+        fmt.Println("----", f, "--------------------")
+
+	pd := pdfread.Load(f)
+        if pd == nil {
+            fmt.Println("can't open input file")
+            fmt.Println()
+            continue
+        }
+
+        fmt.Println("Trailer {")
+        for k, v := range pd.Trailer {
+            fmt.Printf("  %s %q\n", k, v)
+        }
+        fmt.Println("}")
+        fmt.Println()
+
+
+        printobj(pd, pd.Trailer["/Root"], "", "/Root")
+        fmt.Println()
+    }
+}
