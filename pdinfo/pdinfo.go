3a31bb7ae06f1f699362e04fe6db8f19f1c6952a
Raffaele Sena <raff367@gmail.com>
2016-01-13 08:48:04 -0800

    Added debug and dump options

--- pdinfo/pdinfo.go
+++ pdinfo/pdinfo.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	"github.com/raff/pdfreader/pdfread"
+	"github.com/raff/pdfreader/util"
 )
 
-// Example program for pdfread.go
+var (
+	debugobj = false
+)
 
 // The program takes a PDF file as argument and recursively dump the content.
 
@@ -32,28 +35,25 @@ func printobj(pd *pdfread.PdfReaderT, o []byte, indent, prefix string) {
 		return
 	}
 
-	/*
-	   if l < 2 {
-	       fmt.Printf("%s%s %s\n", indent, prefix, "<<invalid>>")
-	       return
-	   }
-	*/
+	if debugobj {
+		fmt.Printf("%q\n", o)
+	}
 
-	switch o[0] {
-	case '[': // array
+	switch {
+	case o[0] == '[': // array
 		a := pdfread.Array(o)
 
 		fmt.Printf("%s%s %s\n", indent, prefix, "[")
 		indent += "  "
 
 		for i, v := range a {
-			printobj(pd, v, indent, fmt.Sprintf("%2d:", i))
+			printobj(pd, v, indent, fmt.Sprintf("%d:", i))
 		}
 
 		indent = indent[2:]
 		fmt.Printf("%s]\n", indent)
 
-	case '<': // dictionary
+	case o[0] == '<' && o[1] == '<': // dictionary
 		d := pdfread.Dictionary(o)
 
 		fmt.Printf("%s%s %s\n", indent, prefix, "{")
@@ -76,7 +76,12 @@ func printobj(pd *pdfread.PdfReaderT, o []byte, indent, prefix string) {
 }
 
 func main() {
-	for _, f := range os.Args[1:] {
+	flag.BoolVar(&util.Debug, "debug", false, "enable debug logging")
+	flag.BoolVar(&debugobj, "dump", false, "dump object content")
+
+	flag.Parse()
+
+	for _, f := range flag.Args() {
 		fmt.Println("----", f, "--------------------")
 
 		pd := pdfread.Load(f)
@@ -93,7 +98,9 @@ func main() {
 		fmt.Println("}")
 		fmt.Println()
 
-		printobj(pd, pd.Trailer["/Root"], "", "/Root")
+		root := pd.Trailer["/Root"]
+
+		printobj(pd, root, "", "/Root")
 		fmt.Println()
 	}
 }
