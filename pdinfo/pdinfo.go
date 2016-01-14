99565e4622212047882c7cd067c358856749ddfc
Raffaele Sena <raff367@gmail.com>
2016-01-14 11:33:04 -0800

    Moved pdinfo printobj in pdutil.Printobj so it can be used by other apps

--- pdinfo/pdinfo.go
+++ pdinfo/pdinfo.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/raff/pdfreader/pdfread"
+	"github.com/raff/pdfreader/pdutil"
 	"github.com/raff/pdfreader/util"
 )
 
@@ -14,90 +15,6 @@ var (
 	debugobj = false
 	maxlevel = 0
 )
-
-func printobj(pd *pdfread.PdfReaderT, o []byte, indent, prefix string, maxlevel int) {
-	maxlevel -= 1
-	l := len(o)
-
-	if l > 2 {
-		if o[l-2] == ' ' && o[l-1] == 'R' { // reference
-			ref := fmt.Sprintf("<<%s>>", o)
-			o = pd.Obj(o)
-
-			if prefix == "" {
-				prefix = ref
-			} else {
-				prefix += " " + ref
-			}
-		}
-	}
-
-	if l == 0 {
-		fmt.Printf("%s%s %s\n", indent, prefix, "<<empty>>")
-		return
-	}
-
-	/*
-	   if l < 2 {
-	       fmt.Printf("%s%s %s\n", indent, prefix, "<<invalid>>")
-	       return
-	   }
-	*/
-
-	if debugobj {
-		fmt.Println(string(o))
-	}
-
-	switch o[0] {
-	case '[': // array
-		a := pdfread.Array(o)
-
-		fmt.Printf("%s%s %s\n", indent, prefix, "[")
-		indent += "  "
-
-		if maxlevel < 0 {
-			fmt.Printf("%s<<more>>\n", indent)
-		} else {
-			for i, v := range a {
-
-				printobj(pd, v, indent, fmt.Sprintf("%d:", i), maxlevel)
-			}
-		}
-
-		indent = indent[2:]
-		fmt.Printf("%s]\n", indent)
-
-	case '<': // dictionary
-		d := pdfread.Dictionary(o)
-
-		fmt.Printf("%s%s %s\n", indent, prefix, "{")
-		indent += "  "
-
-		if maxlevel < 0 {
-			fmt.Printf("%s<<more>>\n", indent)
-		} else {
-			for k, v := range d {
-				if k == "/Parent" { // backreference - don't follow
-					fmt.Printf("%s%s <<%s>>\n", indent, k, string(v))
-				} else {
-					printobj(pd, v, indent, k, maxlevel)
-				}
-			}
-		}
-
-		indent = indent[2:]
-		fmt.Printf("%s}\n", indent)
-
-	case '/': // symbol
-		fmt.Printf("%s%s %s\n", indent, prefix, util.Unescape(o))
-
-	case '(': // string
-		fmt.Printf("%s%s %q\n", indent, prefix, util.String(o))
-
-	default:
-		fmt.Printf("%s%s %s\n", indent, prefix, string(o))
-	}
-}
 
 func main() {
 	flag.BoolVar(&util.Debug, "debug", false, "enable debug logging")
@@ -125,7 +42,7 @@ func main() {
 
 		root := pd.Trailer["/Root"]
 
-		printobj(pd, root, "", "/Root", maxlevel)
+		pdutil.Printobj(pd, root, "", "/Root", maxlevel)
 		fmt.Println()
 	}
 }
---
+++ pdutil/pdutil.go
@@ -0,0 +1,87 @@
+package pdutil
+
+import (
+        "fmt"
+
+	"github.com/raff/pdfreader/pdfread"
+	"github.com/raff/pdfreader/util"
+)
+
+func Printobj(pd *pdfread.PdfReaderT, o []byte, indent, prefix string, maxlevel int) {
+	maxlevel -= 1
+	l := len(o)
+
+	if l > 2 {
+		if o[l-2] == ' ' && o[l-1] == 'R' { // reference
+			ref := fmt.Sprintf("<<%s>>", o)
+			o = pd.Obj(o)
+
+			if prefix == "" {
+				prefix = ref
+			} else {
+				prefix += " " + ref
+			}
+		}
+	}
+
+	if l == 0 {
+		fmt.Printf("%s%s %s\n", indent, prefix, "<<empty>>")
+		return
+	}
+
+	/*
+	if debugobj {
+		fmt.Println(string(o))
+	}
+        */
+
+	switch o[0] {
+	case '[': // array
+		a := pdfread.Array(o)
+
+		fmt.Printf("%s%s %s\n", indent, prefix, "[")
+		indent += "  "
+
+		if maxlevel < 0 {
+			fmt.Printf("%s<<more>>\n", indent)
+		} else {
+			for i, v := range a {
+
+				Printobj(pd, v, indent, fmt.Sprintf("%d:", i), maxlevel)
+			}
+		}
+
+		indent = indent[2:]
+		fmt.Printf("%s]\n", indent)
+
+	case '<': // dictionary
+		d := pdfread.Dictionary(o)
+
+		fmt.Printf("%s%s %s\n", indent, prefix, "{")
+		indent += "  "
+
+		if maxlevel < 0 {
+			fmt.Printf("%s<<more>>\n", indent)
+		} else {
+			for k, v := range d {
+				if k == "/Parent" { // backreference - don't follow
+					fmt.Printf("%s%s <<%s>>\n", indent, k, string(v))
+				} else {
+					Printobj(pd, v, indent, k, maxlevel)
+				}
+			}
+		}
+
+		indent = indent[2:]
+		fmt.Printf("%s}\n", indent)
+
+	case '/': // symbol
+		fmt.Printf("%s%s %s\n", indent, prefix, util.Unescape(o))
+
+	case '(': // string
+		fmt.Printf("%s%s %q\n", indent, prefix, util.String(o))
+
+	default:
+		fmt.Printf("%s%s %s\n", indent, prefix, string(o))
+	}
+}
