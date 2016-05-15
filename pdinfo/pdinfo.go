a53de49ef448f5697f1d91e6f6ecc5570d123090
Raffaele Sena <raff367@gmail.com>
2016-05-15 13:29:11 -0700

    -r option prints referenced resource

--- pdinfo/pdinfo.go
+++ pdinfo/pdinfo.go
@@ -5,7 +5,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/raff/pdfreader/pdfread"
 	"github.com/raff/pdfreader/pdutil"
@@ -16,10 +18,22 @@ var (
 	maxlevel = 0
 )
 
+func PrintRef(w io.Writer, pd *pdfread.PdfReaderT, ref string, fmtref string) {
+	if !strings.HasSuffix(ref, " R") {
+		ref = fmt.Sprintf("%v 0 R", ref)
+	}
+
+	obj := pd.Obj([]byte(ref))
+
+	// print resource info
+	pdutil.Printobj(w, pd, obj, "", ref, maxlevel, fmtref)
+}
+
 func main() {
 	flag.BoolVar(&util.Debug, "debug", false, "enable debug logging")
 	flag.BoolVar(&pdutil.Debugobj, "dump", false, "dump object content")
 	flag.IntVar(&maxlevel, "levels", 5, "maximum number of levels")
+	displayref := flag.String("r", "", "display resource by reference")
 
 	flag.Parse()
 
@@ -36,21 +50,13 @@ func main() {
 		fmt.Println(pd.Version)
 		fmt.Println()
 
+		if *displayref != "" {
+			PrintRef(os.Stdout, pd, *displayref, "")
+			fmt.Println()
+			break
+		}
+
 		pdutil.Printdic(os.Stdout, pd, pd.Trailer, "", "/Trailer", maxlevel, "")
 		fmt.Println()
-
-		/*
-			fmt.Println("Trailer {")
-			for k, v := range pd.Trailer {
-				fmt.Printf("  %s %q\n", k, v)
-			}
-			fmt.Println("}")
-			fmt.Println()
-
-			root := pd.Trailer["/Root"]
-
-			pdutil.Printobj(os.Stdout, pd, root, "", "/Root", maxlevel)
-			fmt.Println()
-		*/
 	}
 }
