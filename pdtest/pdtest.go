ba0c2c661a2ee7f9dfa0909727226aa04c3d0f46
Raffaele Sena <raff367@gmail.com>
2016-05-15 13:27:39 -0700

    print outlines

--- pdtest/pdtest.go
+++ pdtest/pdtest.go
@@ -14,7 +14,23 @@ import (
 func main() {
 	pd := pdfread.Load(os.Args[1])
 	if pd != nil {
+		fmt.Println("PageMode", pd.PageMode)
+		fmt.Println()
+
+		outlines := pd.Outlines()
+
+		if len(outlines) > 0 {
+			fmt.Println("Outlines:")
+			for _, p := range outlines {
+				fmt.Println("Page", p.Page+1, "-", p.Title)
+			}
+
+			fmt.Println()
+		}
+
+		fmt.Println("Pages:")
 		pg := pd.Pages()
+
 		for k := range pg {
 			fmt.Printf("Page %d - MediaBox: %s\n",
 				k+1, pd.Att("/MediaBox", pg[k]))
